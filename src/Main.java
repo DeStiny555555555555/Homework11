ac6f25abe3efb1f90bac40d0a8186768b4e8fa22
DeStiny555555555555 <nik.okhrimenko.2000@mail.ru>
2024-10-31 07:12:32 +0300

    Complete Homework

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void printYear(int year) {
-        if (year > 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год - високосный");
         } else {
             System.out.println(year + " год -  не високосный");
@@ -8,21 +8,16 @@ public class Main {
     }
 
     public static void printDownloadCorrectApp(int clientDeviceYear, int phoneSystems) {
-        if (phoneSystems == 0) {
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для IOS по ссылке");
-            } else {
+            if (phoneSystems == 0 && clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для IOS по ссылке");
-            }
-                }
-        if (phoneSystems == 1) {
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            } else {
+            } else if (phoneSystems == 0 && clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для IOS по ссылке");
+            } else if (phoneSystems == 1 && clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
             }
-        }
-    }
+            }
 
     public static int deliveryDistance (int km) {
         if (km <= 20) {
@@ -38,17 +33,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int year = 2025;
+        int year = 2024;
         printYear(year);
         System.out.println("Задание 2");
-        int clientDeviceYear = 2014;
-        int phoneSystems = 0;
+        int clientDeviceYear = 2015;
+        int phoneSystems = 1;
         printDownloadCorrectApp(clientDeviceYear, phoneSystems);
         System.out.println("Задание 3");
         int deliveryDistance1 = 100;
-        int deliveryDay = deliveryDistance(deliveryDistance1);
-        if (deliveryDay > 0) {
-            System.out.println("Потребуется дней: " + deliveryDay);
+        int calculatingDeliveryDays = deliveryDistance(deliveryDistance1);
+        if (calculatingDeliveryDays > 0) {
+            System.out.println("Потребуется дней: " + calculatingDeliveryDays);
         } else {
             System.out.println("Доставки нет");
         }
